8bc3c9259e4e859cc37c50dc4ad8b58d71a53a5a
JI, Yanjiang <jiyanjiang@live.com>
2015-01-05 18:38:54 +0800

    Replace master branch with page content via GitHub

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,1 @@
 console.log('This would be the main JS file.');
-
-<script type="text/x-mathjax-config">
-  MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
-</script>
-<script type="text/javascript"
-  src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
-</script>
